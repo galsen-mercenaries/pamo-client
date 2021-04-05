568ea0e4024a377b009e3d976b0e0fc21bd5a320
ext_kebe04 <tyny95@outlook.fr>
2021-04-05 22:11:27 +0000

    integration dashboard desktop

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,17 @@ import { CommonModule } from "@angular/common";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./pages/home/home.component";
 import { MatIconModule, MatMenuModule } from "@angular/material";
-import { DashboardHomeComponent } from './pages/dashboard-home/dashboard-home.component';
+import { DashboardHomeComponent } from "./pages/dashboard-home/dashboard-home.component";
+import { SwiperModule } from "ngx-swiper-wrapper";
 
 @NgModule({
   declarations: [HomeComponent, DashboardHomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MatMenuModule, MatIconModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatMenuModule,
+    MatIconModule,
+    SwiperModule,
+  ],
 })
 export class HomeModule {}
--- src/app/home/pages/dashboard-home/dashboard-home.component.ts
+++ src/app/home/pages/dashboard-home/dashboard-home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SwiperConfigInterface } from "ngx-swiper-wrapper";
+import { SWIPER_CONFIGURATION } from "src/app";
 
 @Component({
-  selector: 'app-dashboard-home',
-  templateUrl: './dashboard-home.component.html',
-  styleUrls: ['./dashboard-home.component.scss']
+  selector: "app-dashboard-home",
+  templateUrl: "./dashboard-home.component.html",
+  styleUrls: ["./dashboard-home.component.scss"],
 })
 export class DashboardHomeComponent implements OnInit {
+  appointments = [
+    { date: "Lun 1 Avr", percent: 0 },
+    { date: "Mer 10 Avr", percent: 100 / 3 },
+    { date: "Jeu 18 Avr", percent: 60 },
+    { date: "Mar 30 Avr", percent: 100 },
+  ];
+  images = [
+    "/assets/images/healthy-food.jpg",
+    "/assets/images/sports.jpg",
+    "/assets/images/mask.jpg",
+  ];
+  swiperConfig: SwiperConfigInterface = {
+    direction: "vertical",
+    slidesPerView: 1.6,
+    keyboard: false,
+    mousewheel: false,
+    scrollbar: false,
+    navigation: false,
+    pagination: false,
+    autoplay: true,
+    loop: true,
+    speed: 1200,
+    spaceBetween: 10,
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
