cd107eac714c3c034a0c095daa5a142e3b7a9ec6
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2022-01-07 21:47:41 +0000

    update @angular/cdk version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  // SERVER_URL: "http://pamo-back-end-dev.us-east-1.elasticbeanstalk.com",
-  SERVER_URL: "http://ec2-15-188-193-194.eu-west-3.compute.amazonaws.com:8080",
+    production: false,
+    // SERVER_URL: 'http://[::1]:8080'
+    SERVER_URL: 'http://ec2-15-188-193-194.eu-west-3.compute.amazonaws.com:8080'
 };
 
 /*
