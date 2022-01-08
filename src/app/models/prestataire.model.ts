4d5f599c41389b100b2acd12d1210489ee767043
papakebe95 <tyny95@outlook.fr>
2022-01-08 10:56:14 +0000

    implementer recuperation des prestataires

---
+++ src/app/models/prestataire.model.ts
@@ -0,0 +1,10 @@
+export interface PrestataireModel {
+  prestataireId?: number;
+  nom?: string;
+  libelle?: string;
+  adresse?: string;
+  longitude?: number;
+  latitude?: number;
+  numero?: string;
+  status?: true;
+}
--- src/app/services/news-service/news.service.ts
+++ src/app/services/news-service/news.service.ts
@@ -2,9 +2,11 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map } from "rxjs/operators";
 import { NewsModel } from "src/app/models/news.model";
+import { PrestataireModel } from "src/app/models/prestataire.model";
 import { environment } from "src/environments/environment";
 const { SERVER_URL } = environment;
 const NEWS_URL = `${SERVER_URL}/news`;
+const PRESTATAIRES_URL = `${SERVER_URL}/prestataires`;
 @Injectable({
   providedIn: "root",
 })
@@ -17,5 +19,9 @@ export class NewsService {
         return res.concat(res).concat(res);
       })
     );
+  }
+
+  getAllPrestataires() {
+    return this.http.get<PrestataireModel[]>(PRESTATAIRES_URL);
   }
 }
